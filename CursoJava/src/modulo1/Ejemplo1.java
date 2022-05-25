bce90d4e167399807b7c073887c55b4fc088bc0a
Javier Fernandez Fernandez <javier.fernandezf@allianz.es>
2022-05-25 14:00:02 +0200

    primera version

---
+++ CursoJava/src/modulo1/Ejemplo1.java
@@ -0,0 +1,13 @@
+package modulo1;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+		System.out.println("\njavier \tFernandez");
+		System.out.println("\njavier\thttps://github.com/fjavif77/Cursojava2022fjavif");
+		System.out.println("para las comillas \" para las barras \\ ");
+
+	}
+
+}
