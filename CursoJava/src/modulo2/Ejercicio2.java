57577cd0446ecbde32799a4b4f0e40fc2734d9c3
Javier Fernandez Fernandez <javier.fernandezf@allianz.es>
2022-05-26 09:42:32 +0200

    añado ejercicios dos y tres

---
+++ CursoJava/src/modulo2/Ejercicio2.java
@@ -0,0 +1,17 @@
+package modulo2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Nombre\t\tapellido\t\temail\t\t\t\tteléfono");
+		System.out.println("------\t\t--------\t\t-----\t\t\t\t--------");
+		System.out.println("\nJordi\t\tGuirao\t\t\tjordi.guirao@allianz.es\t\t661598657");
+		System.out.println("\nSergio\t\tNavarro\t\t\tsergio.navarro@allianz.es\t661359657");
+		System.out.println("\nAdrian\t\tMolina\t\t\tadrian.molina@allianz.es\t661326854");
+		System.out.println("\nIvan\t\tYepes\t\t\tivan.yepes@allianz.es\t\t663251485");
+		System.out.println("\nLiliana\t\tMendoza\t\t\tliliana.mendoza@allianz.es\t664265235");
+		System.out.println("\nRicard\t\tBoix\t\t\tricard.boix@allianz.es\t\t661254689");
+		System.out.println("\nOscar\t\tGou\t\t\toscar.gou@allianz.es\t\t661253485");
+	}
+
+}
---
+++ CursoJava/src/modulo3/Ejercicio3.java
@@ -0,0 +1,17 @@
+package modulo3;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		System.out.println("Tecla de escape\t\tsignificado");
+		System.out.println("\\n\t\t\tsignifica una nueva linea");
+		System.out.println("\\t\t\t\tsignifica un tab de espacio");
+		System.out.println("\\\"\t\t\tes para poner \"(comillas dobles)"
+				+ " dentro del texto por ejemplo \"Belencita\"");
+		System.out.println("\\\\\t\t\tse utiliza para escribir la \\ "
+				+ "dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\\\'\t\t\tse utiliza para las \'(comilla"
+				+ " simple) para escribir por ejemplo \'Princesita\'");
+	}
+
+}
